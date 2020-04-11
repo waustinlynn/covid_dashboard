1510c74ae118d704b3b125e2d801f67741971ad9
nthrive\alynn <alynn@nthrive.com>
2020-04-11 07:14:31 -0400

    added build step to data import

--- importdata.sh
+++ importdata.sh
@@ -1,3 +1,4 @@
 curl https://covidtracking.com/api/v1/states/daily.json > ./src/data/statesdaily.json
 curl https://covidtracking.com/api/us/daily > ./src/data/usdaily.json
 curl https://covidtracking.com/api/v1/states/current.json > ./src/data/statescurrent.json
+ng build --prod
